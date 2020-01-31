c89ce6975dc644aa760e59e935d5988288472409
Patrick Göler von Ravensburg <mail@pgvr.dev>
2020-01-31 17:03:10 +0100

    tailwind

---
+++ webpack.prod.config.js
@@ -0,0 +1,27 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.html", "./src/**/*.component.ts"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: "postcss-loader",
+        options: {
+          ident: "postcss",
+          syntax: "postcss-scss",
+          plugins: () => [
+            require("postcss-import"),
+            require("tailwindcss"),
+            require("autoprefixer"),
+            purgecss
+          ]
+        }
+      }
+    ]
+  }
+};
